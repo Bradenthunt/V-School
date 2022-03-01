10c32fb169e9c4e0d7d890a5851030819d8579f6
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2022-02-28 19:34:20 -0700

    level 2 assignments

---
+++ mario-pest-control/main.js
@@ -0,0 +1,15 @@
+const form = document['pest-cost']
+
+form.addEventListener("submit", pestControlTotal)
+
+function pestControlTotal(e) {
+    e.preventDefault()
+    let goombaTotal = form.goombas.value * 5
+    let bobombsTotal = form.bobombs.value * 7
+    let cheepsTotal = form.cheeps.value * 11
+
+    let totalCost = goombaTotal + bobombsTotal + cheepsTotal
+
+    // document.getElementById('total').textContent = `The total cost for your requested services will be ${totalCost} coins.`
+    alert(`The total cost for your requested services will be ${totalCost} coins.`)
+}
