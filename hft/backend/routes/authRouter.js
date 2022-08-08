a8073162caddc39463ec02c8e5f6bd36969ec49f
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2022-08-08 14:02:47 -0600

    more hft

--- hft/backend/routes/authRouter.js
+++ hft/backend/routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const authRouter = express.Router()
-const User = require('../models/User')
+const User = require('../models/User.js')
 const jwt = require('jsonwebtoken')
 
 
