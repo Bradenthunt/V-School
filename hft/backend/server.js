21972dd60768776d792f4f5381bc752a46709269
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2022-08-08 13:25:38 -0600

    hft updates

---
+++ hft/backend/server.js
@@ -0,0 +1,54 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const { expressjwt } = require('express-jwt')
+const path = require('path')
+
+
+process.env.SECRET
+
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+const port = process.env.PORT || 9000
+
+
+mongoose.connect(
+    process.env.mongoURI,
+  () => console.log('Connected to DB')
+)
+
+
+app.use('/auth', require(path.join(__dirname, 'routes', 'authRouter')))
+app.use('/api', expressjwt({secret: process.env.SECRET, algorithms: ['HS256']}))
+app.use('/api/workouts', require(path.join(__dirname, 'routes', 'workoutRouter')))
+app.use('/api/exercises', require(path.join(__dirname, 'routes', 'exerciseRouter')))
+
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  if(err.name === "UnauthorizedError") {
+    res.status(err.status)
+  }
+  return res.send({errMsg: err.message})
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running on 9000`)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+  })
+}
